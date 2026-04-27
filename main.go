84bd0b32d66b923e4a6fc659594569db6aed562f
bear1278 <grigorijanusik@gmail.com>
2026-04-27 23:33:30 +0300

    refactor

--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+type SimpleArchiver struct {
+	inputPath  string
+	outputPath string
+	buffer     []byte
+}
+
+func NewSimpleArchiver(inputPath string) *SimpleArchiver {
+	buffer := make([]byte, 1024*8)
+	return &SimpleArchiver{inputPath: inputPath, buffer: buffer}
+}
+
 func main() {
 
 }
