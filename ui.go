3bda264d992707006719e8e44e33005f79936e8f
bear1278 <grigorijanusik@gmail.com>
2026-04-28 16:47:50 +0300

    add menu

--- ui.go
+++ ui.go
@@ -1,6 +1,10 @@
 package main
 
-import tea "github.com/charmbracelet/bubbletea"
+import (
+	"fmt"
+	tea "github.com/charmbracelet/bubbletea"
+	"strings"
+)
 
 type model struct {
 	archiver  *SimpleArchiver
@@ -13,7 +17,7 @@ type model struct {
 
 func initialModel() model {
 	return model{
-		// Our to-do list is a grocery list
+		state:   "menu",
 		choices: []string{"Сжать файл", "Распаковать файл", "Выход"},
 	}
 }
@@ -24,5 +28,22 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 func (m model) View() string {
+	if m.state == "menu" {
+		return m.viewMenu()
+	}
 	return ""
+}
+
+func (m model) viewMenu() string {
+	builder := strings.Builder{}
+	builder.WriteString("===== Архиватор =====\n\n")
+	for i, choice := range m.choices {
+		cursor := " "
+		if m.cursor == i {
+			cursor = ">"
+		}
+		builder.WriteString(fmt.Sprintf("%s %s \n", cursor, choice))
+	}
+	builder.WriteString("Используйте стрелки для навигации и enter для выбора\nНажмите q для выхода")
+	return builder.String()
 }
